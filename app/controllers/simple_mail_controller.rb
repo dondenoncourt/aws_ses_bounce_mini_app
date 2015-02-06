1dda973ab08b1d8e48db273de3214bfee83a9341
dondenoncourt <dondenoncourt@gmail.com>
2015-02-06 08:27:57 -0500

    email options

--- app/controllers/simple_mail_controller.rb
+++ app/controllers/simple_mail_controller.rb
@@ -10,9 +10,15 @@ class SimpleMailController < ApplicationController
   def bounce
     puts "it bounced"
     json = JSON.parse(request.raw_post)
-    if confirmation_url = json['SubscribeURL']
-      _confirm_sns_subscription(confirmation_url)
-    else # process bounce
+    aws_needs_url_confirmed = json['SubscribeURL']
+    if aws_needs_url_confirmed
+      uri = URI.parse(aws_needs_url_confirmed)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      response = http.get(uri.request_uri)
+      puts "could not confirm aws sns subscription url from: #{@data}" unless response.code.to_s == '200'
+    else # handle AWS bounce callback
       message = json['Message']
       message = json if message.nil? # AWS SNS payload on dev configuration did not have a Message element
       raise "could not parse aws sns bounce message from #{json}" if message.empty?
@@ -20,15 +26,6 @@ class SimpleMailController < ApplicationController
       Notification.process_aws_email_bounces(message)
     end
     render nothing: true, status: 200
-  end
-private
-  def _confirm_sns_subscription(confirmation_url)
-    uri = URI.parse(confirmation_url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    response = http.get(uri.request_uri)
-    puts "could not confirm aws sns subscription url from: #{@data}" unless response.code.to_s == '200'
   end
 
 end
