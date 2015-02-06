b0bd9a5fab80250b2e97d24c254053fc7ffe8838
dondenoncourt <dondenoncourt@gmail.com>
2015-02-06 16:46:37 -0500

    email options

--- app/controllers/simple_mail_controller.rb
+++ app/controllers/simple_mail_controller.rb
@@ -2,6 +2,7 @@ class SimpleMailController < ApplicationController
   skip_before_filter :verify_authenticity_token # need this for AWS callbacks
 
   def mail_it
+    logger.info "mail_it called with #{params}"
     @email = params[:email]
     SimpleMailer.mail_it(@email, 'original text').deliver
     render text: 'mail sent'
@@ -9,9 +10,10 @@ class SimpleMailController < ApplicationController
 
   def bounce
     json = JSON.parse(request.raw_post)
+    logger.info "bounce callback from AWS with #{json}"
     aws_needs_url_confirmed = json['SubscribeURL']
     if aws_needs_url_confirmed
-      puts "AWS is requesting confirmation of the bounce handler URL"
+      logger.info "AWS is requesting confirmation of the bounce handler URL"
       uri = URI.parse(aws_needs_url_confirmed)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
@@ -19,8 +21,11 @@ class SimpleMailController < ApplicationController
       response = http.get(uri.request_uri)
       puts "could not confirm aws sns subscription url from: #{@data}" unless response.code.to_s == '200'
     else
-      puts "AWS has sent us a bounce notification: #{json}"
+      logger.info "AWS has sent us a bounce notification: #{json}"
       SimpleMailer.mail_it('dondenoncourt@gmail.com', json).deliver
+      json['bounce']['bouncedRecipients'].each do |recipient|
+        logger.info "bounce on send to #{recipient['emailAddress']}"
+      end
     end
     render nothing: true, status: 200
   end
