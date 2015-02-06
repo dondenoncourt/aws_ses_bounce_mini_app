cb6456ec667d9fa27decc7a2716cd9e2caa548d1
dondenoncourt <dondenoncourt@gmail.com>
2015-02-06 08:17:48 -0500

    email options

--- app/controllers/simple_mail_controller.rb
+++ app/controllers/simple_mail_controller.rb
@@ -1,4 +1,6 @@
 class SimpleMailController < ApplicationController
+  skip_before_filter :verify_authenticity_token # need this for AWS callbacks
+
   def mail_it
     @email = params[:email]
     SimpleMailer.mail_it(@email).deliver
@@ -11,9 +13,9 @@ class SimpleMailController < ApplicationController
     if confirmation_url = json['SubscribeURL']
       _confirm_sns_subscription(confirmation_url)
     else # process bounce
-      message = @data['Message']
-      message = @data if message.nil? # AWS SNS payload on dev configuration did not have a Message element
-      raise "could not parse aws sns bounce message from #{@data}" if message.empty?
+      message = json['Message']
+      message = json if message.nil? # AWS SNS payload on dev configuration did not have a Message element
+      raise "could not parse aws sns bounce message from #{json}" if message.empty?
       User.process_aws_email_bounces(message)
       Notification.process_aws_email_bounces(message)
     end
