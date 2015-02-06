a44fb2d5ce6e7b7471b12f69afbefa4fdb882b68
dondenoncourt <dondenoncourt@gmail.com>
2015-02-06 08:52:40 -0500

    email options

--- app/controllers/simple_mail_controller.rb
+++ app/controllers/simple_mail_controller.rb
@@ -28,4 +28,25 @@ class SimpleMailController < ApplicationController
     render nothing: true, status: 200
   end
 
+  def complaint
+    puts "processing complaint"
+    json = JSON.parse(request.raw_post)
+    aws_needs_url_confirmed = json['SubscribeURL']
+    if aws_needs_url_confirmed
+      uri = URI.parse(aws_needs_url_confirmed)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      response = http.get(uri.request_uri)
+      puts "could not confirm aws sns subscription url from: #{@data}" unless response.code.to_s == '200'
+    else # handle AWS bounce callback
+      message = json['Message']
+      message = json if message.nil? # AWS SNS payload on dev configuration did not have a Message element
+      raise "could not parse aws sns bounce message from #{json}" if message.empty?
+      User.process_aws_email_bounces(message)
+      Notification.process_aws_email_bounces(message)
+    end
+    render nothing: true, status: 200
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'mail_it' => 'simple_mail#mail_it'
   post 'aws_sns/bounce' => 'simple_mail#bounce'
+  post 'aws_sns/complaint' => 'simple_mail#complaint'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
