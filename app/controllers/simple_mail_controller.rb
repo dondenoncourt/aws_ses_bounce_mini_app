9f5c4a3c4e3d267b46220f9e5be1bc6ebcbea6bb
dondenoncourt <dondenoncourt@gmail.com>
2015-02-06 12:36:36 -0500

    email options

--- app/controllers/simple_mail_controller.rb
+++ app/controllers/simple_mail_controller.rb
@@ -3,48 +3,24 @@ class SimpleMailController < ApplicationController
 
   def mail_it
     @email = params[:email]
-    SimpleMailer.mail_it(@email).deliver
+    SimpleMailer.mail_it(@email, 'original text').deliver
     render text: 'mail sent'
   end
 
   def bounce
-    puts "it bounced"
     json = JSON.parse(request.raw_post)
     aws_needs_url_confirmed = json['SubscribeURL']
     if aws_needs_url_confirmed
+      puts "AWS is requesting confirmation of the bounce handler URL"
       uri = URI.parse(aws_needs_url_confirmed)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       response = http.get(uri.request_uri)
       puts "could not confirm aws sns subscription url from: #{@data}" unless response.code.to_s == '200'
-    else # handle AWS bounce callback
-      message = json['Message']
-      message = json if message.nil? # AWS SNS payload on dev configuration did not have a Message element
-      raise "could not parse aws sns bounce message from #{json}" if message.empty?
-      User.process_aws_email_bounces(message)
-      Notification.process_aws_email_bounces(message)
-    end
-    render nothing: true, status: 200
-  end
-
-  def complaint
-    puts "processing complaint"
-    json = JSON.parse(request.raw_post)
-    aws_needs_url_confirmed = json['SubscribeURL']
-    if aws_needs_url_confirmed
-      uri = URI.parse(aws_needs_url_confirmed)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      response = http.get(uri.request_uri)
-      puts "could not confirm aws sns subscription url from: #{@data}" unless response.code.to_s == '200'
-    else # handle AWS bounce callback
-      message = json['Message']
-      message = json if message.nil? # AWS SNS payload on dev configuration did not have a Message element
-      raise "could not parse aws sns bounce message from #{json}" if message.empty?
-      User.process_aws_email_bounces(message)
-      Notification.process_aws_email_bounces(message)
+    else
+      puts "AWS has sent us a bounce notification: #{json}"
+      SimpleMailer.mail_it('dondenoncourt@gmail.com', json).deliver
     end
     render nothing: true, status: 200
   end
--- app/mailers/simple_mailer.rb
+++ app/mailers/simple_mailer.rb
@@ -1,7 +1,8 @@
 class SimpleMailer < ActionMailer::Base
   default from: "dondenoncourt@gmail.com"
-  def mail_it(email)
+  def mail_it(email, body_text)
     @email = email
+    @body_text = body_text
     mail(to: email, subject: 'Simple test of AWS SES')
   end
 end
