1fc74bc2a8f90c30875630993286948634ec10c0
dondenoncourt <dondenoncourt@gmail.com>
2015-02-07 16:01:53 -0500

    env vars

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -1,4 +1,4 @@
 # don't put your keys in free text
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id: 'AKIAI2SX6L4FPN7BVXPA',
-  secret_access_key: 'eJA/4eT8FmWGweAfEOhCz9b18XGDq1o46rAW2NQa'
+  access_key_id: ENV['ACCESS_KEY_ID'],
+  secret_access_key: ENV['SECRET_ACCESS_KEY']
